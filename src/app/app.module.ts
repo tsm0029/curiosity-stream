d96179d66cc67790029264e3c6d1ab9c87ff6f0c
Travis <tsm0029@mix.wvu.edu>
2022-01-25 15:01:55 -0500

    more nav and footer changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { headerComponent } from './header/header.component';
+import { footerComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    headerComponent
+    headerComponent,
+    footerComponent
 
   ],
   imports: [
